bdd3bae70f59854725824bcfc34e39309f10f56e
harshiniadvik <v.harshini1817@gmail.com>
2025-06-11 19:20:09 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,4 +1,4 @@
-public class sampletest {
+public class sample {
     public static void main(String[] args) {
         // Replace "Your Name" with your actual name
         System.out.println("harshini");
